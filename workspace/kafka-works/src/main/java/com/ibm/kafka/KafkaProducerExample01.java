b7e303dc5e7ffe1a99ae77e62eec819a4a1b4326
Naveen <adith.naveen@gmail.com>
2023-02-07 16:32:16 +0530

    working with kafka java

--- workspace/kafka-works/src/main/java/com/ibm/kafka/KafkaProducerExample01.java
+++ workspace/kafka-works/src/main/java/com/ibm/kafka/KafkaProducerExample01.java
@@ -1,5 +1,10 @@
 package com.ibm.kafka;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +14,15 @@ public class KafkaProducerExample01 {
 	public static void main(String args[]) {
 		final String PORT = "9092"; 
 		final String HOST = "localhost"; 
+		
+		Properties prop = new Properties();
+		
+//		prop.put("bootstrap.servers", HOST +":" + PORT);
+		prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG , HOST +":" + PORT);
+		prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); 
+		prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		
+		KafkaProducer<String, String> producer = new KafkaProducer<>(prop);
 		
 		
 	}
